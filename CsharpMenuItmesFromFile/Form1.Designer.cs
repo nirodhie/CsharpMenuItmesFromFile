b75445f9e1984157bfebfc3a7481fbcaee7e6c9d
nirodhie <lteska2@gmail.com>
2018-11-18 13:56:47 +0100

    working with multiple level config file

--- CsharpMenuItmesFromFile/Form1.Designer.cs
+++ CsharpMenuItmesFromFile/Form1.Designer.cs
@@ -28,13 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "placeholder for name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(188, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "placeholder for command";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- CsharpMenuItmesFromFile/Form1.cs
+++ CsharpMenuItmesFromFile/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace CsharpMenuItmesFromFile
 {
@@ -15,6 +16,60 @@ namespace CsharpMenuItmesFromFile
         public Form1()
         {
             InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = nameFromTXTfile("textForLabel.txt", 1);
+            label2.Text = CMDFromTXTfile("textForLabel.txt", 1);
+        }
+
+        public string nameFromTXTfile(string filename,int lineToRead)
+        {
+            try
+            {   // Open the text file using a stream reader.
+                
+                    // Read the stream to a string, and write the string to the console.
+                    String line= File.ReadLines(filename).Skip(lineToRead-1).Take(1).First();
+                    string output = line.Split(';')[0];
+                    return output.ToString();
+
+            }
+            catch (Exception e)
+            {
+               string output = "problem with file "+ filename;
+                MessageBox.Show(e.Message, output);
+                return output;
+            }
+            
+        }
+
+        public string CMDFromTXTfile(string filename, int lineToRead)
+        {
+            try
+            {   // Open the text file using a stream reader.
+
+                // Read the stream to a string, and write the string to the console.
+                String line = File.ReadLines(filename).Skip(lineToRead - 1).Take(1).First();
+                string output = line.Split(';')[1];
+                return output.ToString();
+
+            }
+            catch (Exception e)
+            {
+                string output = "problem with file " + filename;
+                MessageBox.Show(e.Message, output);
+                return output;
+            }
+
+        }
+
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
